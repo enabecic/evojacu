0762577d3b0545c8c26afab69332f004a83b8e92
enabecic <ena.becic@edu.fit.ba>
2024-08-08 13:01:19 +0200

    TASK 39

--- front-end/src/app/pocetna/pocetna.component.ts
+++ front-end/src/app/pocetna/pocetna.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MojConfig } from '../moj-config';
+import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { JezikService } from "../Services/jezik.service";
 
 interface Kategorija {
   kategorijaID: number;
   naziv: string;
   slika: string;
+}
+
+interface Posao {
+  posaoID: number;
+  nazivZadatka: string;
+  cijena: number;
+  zadatakStraniID: number;
+  gradID: number;
+  datumObjave: Date;
 }
 
 @Component({
@@ -18,10 +30,33 @@ export class PocetnaComponent implements OnInit {
   currentPage: number = 0;
   itemsPerPage: number = 5;
 
-  constructor(private http: HttpClient) {}
+  poslovi: Posao[] = [];
+
+  constructor(private http: HttpClient, private router: Router, public jezikService: JezikService) {}
 
   ngOnInit(): void {
     this.getKategorije();
+    this.getPoslovi().subscribe((data: any) => {
+      this.poslovi = this.filterAndSortPoslovi(data.poslovi);
+    });
+  }
+
+  getZadatakSlikaURL(zadatakID: number): string {
+    return `${MojConfig.adresa_servera}/Zadatak/slika?id=${zadatakID}`;
+  }
+
+  getPoslovi(): Observable<any> {
+    return this.http.get<any>(`${MojConfig.adresa_servera}/Posao-preuzmi`);
+  }
+
+  filterAndSortPoslovi(poslovi: Posao[]): Posao[] {
+    return poslovi
+      .sort((a, b) => new Date(b.datumObjave).getTime() - new Date(a.datumObjave).getTime()) // Sortiraj po datumu objave, najnoviji prvo
+      .slice(0, 9); // Uzmi prvih 9 poslova
+  }
+
+  navigateToDetails(id: number): void {
+    this.router.navigate(['/posao-detalji', id]);
   }
 
   get currentKategorije(): Kategorija[] {
