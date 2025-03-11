6e9f4652d93a50d69d4b9b609fd5fcd499294f4d
lamkee <lamija.behram@edu.fit.ba>
2025-03-11 15:16:59 +0100

    82

--- front-end/src/app/Components/korisnicki-profil/korisnicki-profil.component.ts
+++ front-end/src/app/Components/korisnicki-profil/korisnicki-profil.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-korisnicki-profil',
   templateUrl: './korisnicki-profil.component.html',
   styleUrls: ['./korisnicki-profil.component.css']
 })
-export class KorisnickiProfilComponent {
+export class KorisnickiProfilComponent implements OnInit {
+  korisnik: any = {
+    Ime: 'Edogaru',
+    Prezime: 'User',
+    Email: 'edogaru@mail.com.my',
+    Telefon: '',
+    Adresa: ''
+  };
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const prijavljeniKorisnik = this.authService.getTrenutniKorisnik();
+    if (prijavljeniKorisnik) {
+      this.korisnik = prijavljeniKorisnik;
+    }
+  }
+
+  spasiProfil() {
+    alert('Profil sačuvan!');
+  }
+
+  logout() {
+    this.authService.logout();
+    this.korisnik = null;
+    alert('Odjavljeni ste!');
+  }
 }
